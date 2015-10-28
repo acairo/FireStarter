22945301a58b588dd8b0554b837ac66358a22787
Anthony Cairo <acairo@godaddy.com>
2015-10-27 23:48:24 -0700

    added NotImplemented on future reader methods.

--- firestarter/readers.py
+++ firestarter/readers.py
@@ -1,6 +1,15 @@
 
 class Reader(object):
-  pass
+  
+  def read(self):
+    self.data = None
+    raise NotImplementedError
+
+  def head_data(self, lines=None):
+    if not lines:
+      lines = 9
+    return self.data[0:lines]
+
 
 class HttpApi(Reader):
   
